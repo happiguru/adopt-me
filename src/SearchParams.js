c3b81385a1314856bf5ec8a659a421efb02df8af
happiguru <hguruman@gmail.com>
2021-07-15 01:52:05 +0400

    Add api, apply hooks

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
-import Pet from './Pet';
+import useBreedList from './useBreedList';
+import Results from './Results';
 
 const ANIMALS = ["bird", "cat", "dog", "reptile", "rabbit"];
 const SearchParams = () => {
@@ -7,7 +8,7 @@ const SearchParams = () => {
     const [animal, setAnimal] = useState("");
     const [breed, setBreed] = useState("");
     const [pets, setPets] = useState([]);
-    const breeds = [];
+    const [breeds] = useBreedList(animal);
 
     useEffect(() => {
         requestPets();
@@ -25,7 +26,11 @@ const SearchParams = () => {
 
     return (
         <div className="search-params">
-            <form>
+            <form onSubmit={e => {
+                e.preventDefault();
+                requestPets();
+              }}
+            >
                 <label htmlFor="location">
                     Location
                     <input id="location" onChange={e => setLocation(e.target.value)}
@@ -71,13 +76,7 @@ const SearchParams = () => {
                 </label>
                 <button>Submit</button>
             </form>
-            {
-                pets.map(pet => (
-                    <Pet name={pet.name} animal={pet.animal} breed={pet.breed} 
-                    key={pet.id} 
-                />
-                ))
-            }
+            <Results pets={pets} />
         </div>
     )
 }
